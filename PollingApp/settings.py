09b9a03ba3cf563d9a68c39f431bcb95e1006c78
SparshChaurasia <sparshchaurasia0021@gmail.com>
2022-08-08 19:50:21 +0530

    added home route

--- PollingApp/settings.py
+++ PollingApp/settings.py
@@ -115,11 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
-
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = ["static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
