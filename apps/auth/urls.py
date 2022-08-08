1d58fa83733cb9b1322c4aaa52737ae453f23057
SparshChaurasia <sparshchaurasia0021@gmail.com>
2022-08-08 21:03:23 +0530

    added user login/logout

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("RWXddGRprEsVxTaltGzt", views.login_user, name="login"),
+    path("xbriVVkFqxxMQLwouLXq", views.logout_user, name="logout")
 ]
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
     return render(request, "login.html")
+
+
+def login_user(request):
+    if request.method != "POST":
+        return HttpResponse("Bad request method")
+    
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+
+    user = authenticate(request, username=username, password=password)
+    if not user:
+        return HttpResponse("not a user lol")
+
+    login(request, user)
+    return redirect("/vote")
+
+def logout_user(request):
+    logout(request)
+   
+    return redirect("/")
