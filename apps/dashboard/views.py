d292522f1a6bc94a7c2c1800ff70120a06ad782b
SparshChaurasia <sparshchaurasia0021@gmail.com>
2022-08-14 14:13:32 +0530

    created basic dashboard with student status; moved container styling to base css

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+from apps.vote.models import Student
 
 @login_required(login_url="/login")
 def index(request):
-    return render(request, "dashboard.html")
+    if request.method != "POST":
+        return render(request, "dashboard.html")
+    
+    s_class = request.POST.get("class")
+    students = Student.objects.filter(StudentClass=s_class)
+
+    data = {f"student{i}":{"name":s.StudentName, "roll":s.Roll, "status":s.Voted} for i, s in enumerate(students)}
+
+    return render(request, "dashboard.html", {"class": data})
+
+# def status(request):
+#     s_class = request.GET.get("class")
+#     students = Student.objects.filter(StudentClass=s_class)
+#     print(students)
+#     return redirect("/", students=students) 
